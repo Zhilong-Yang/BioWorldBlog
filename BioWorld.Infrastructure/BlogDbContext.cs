54d41ce41c92d00a7122e429c7ffc94743407b96
Zhilong Yang <yzl_200@sina.com>
2020-06-10 21:21:31 +0800

    add DbContest
    add blog dbcontest and common project

---
+++ BioWorld.Infrastructure/BlogDbContext.cs
@@ -0,0 +1,35 @@
+﻿using BioWorld.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BioWorld.Infrastructure
+{
+    public class BlogDbContext : DbContext
+    {
+        protected BlogDbContext()
+        {
+        }
+
+        public BlogDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public virtual DbSet<CategoryEntity> Category { get; set; }
+        public virtual DbSet<CommentEntity> Comment { get; set; }
+        public virtual DbSet<CommentReplyEntity> CommentReply { get; set; }
+        public virtual DbSet<PingbackHistoryEntity> PingbackHistory { get; set; }
+        public virtual DbSet<PostEntity> Post { get; set; }
+        public virtual DbSet<PostCategoryEntity> PostCategory { get; set; }
+        public virtual DbSet<PostExtensionEntity> PostExtension { get; set; }
+        public virtual DbSet<PostPublishEntity> PostPublish { get; set; }
+        public virtual DbSet<PostTagEntity> PostTag { get; set; }
+        public virtual DbSet<TagEntity> Tag { get; set; }
+        public virtual DbSet<FriendLinkEntity> FriendLink { get; set; }
+        public virtual DbSet<CustomPageEntity> CustomPage { get; set; }
+        public virtual DbSet<MenuEntity> Menu { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(typeof(BlogDbContext).Assembly);
+        }
+    }
+}
