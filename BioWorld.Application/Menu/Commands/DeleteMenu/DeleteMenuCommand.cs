49bc2b64b41d49fc7694522bb8b1981315d9e76e
Zhilong Yang <yzl_200@sina.com>
2020-06-27 10:06:06 +0800

    Add Delete Menu Command

---
+++ BioWorld.Application/Menu/Commands/DeleteMenu/DeleteMenuCommand.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using BioWorld.Application.Common.Interface;
+using MediatR;
+
+namespace BioWorld.Application.Menu.Commands.DeleteMenu
+{
+    public class DeleteMenuCommand : IRequest
+    {
+        public Guid Id { get; set; }
+    }
+
+    public class DeleteMenuCommandHandler : IRequestHandler<DeleteMenuCommand>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public DeleteMenuCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Unit> Handle(DeleteMenuCommand request, CancellationToken cancellationToken)
+        {
+            var item = await _context.Menu.FindAsync(request.Id);
+
+            if (null != item)
+            {
+                _context.Menu.Remove(item);
+                await _context.SaveChangesAsync(cancellationToken);
+            }
+
+            return Unit.Value;
+        }
+    }
+}
--- BioWorld.BackEnd/Controllers/MenuController.cs
+++ BioWorld.BackEnd/Controllers/MenuController.cs
@@ -2,6 +2,7 @@
 using System.Threading.Tasks;
 using BioWorld.Application.Menu;
 using BioWorld.Application.Menu.Commands.CreateMenu;
+using BioWorld.Application.Menu.Commands.DeleteMenu;
 using BioWorld.Application.Menu.Queries.GetMenu;
 using BioWorld.Application.Menu.Queries.GetMenus;
 using Microsoft.AspNetCore.Mvc;
@@ -31,6 +32,13 @@ namespace BioWorld.BackEnd.Controllers
         public async Task<ActionResult<MenuDto>> Get(Guid id)
         {
             return Ok(await Mediator.Send(new GetMenuQuery(){Id = id}));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> Delete(Guid id)
+        {
+            await Mediator.Send(new DeleteMenuCommand { Id = id });
+            return NoContent();
         }
     }
 }
