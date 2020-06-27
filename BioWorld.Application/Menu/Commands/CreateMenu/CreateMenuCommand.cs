c9a9892abe2c167e12d7b2360526da2f817ef742
Zhilong Yang <yzl_200@sina.com>
2020-06-27 09:37:48 +0800

    Add Create Menu Command

---
+++ BioWorld.Application/Menu/Commands/CreateMenu/CreateMenuCommand.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using BioWorld.Application.Common.Interface;
+using BioWorld.Domain.Entities;
+using MediatR;
+
+namespace BioWorld.Application.Menu.Commands.CreateMenu
+{
+    public class CreateMenuCommand : IRequest<MenuDto>
+    {
+        public string Title { get; set; }
+
+        public string Url { get; set; }
+
+        public string Icon { get; set; }
+
+        public int DisplayOrder { get; set; }
+
+        public bool IsOpenInNewTab { get; set; }
+    }
+
+    public class CreateMenuCommandHandler : IRequestHandler<CreateMenuCommand, MenuDto>
+    {
+        private readonly IApplicationDbContext _context;
+
+        public CreateMenuCommandHandler(IApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<MenuDto> Handle(CreateMenuCommand request, CancellationToken cancellationToken)
+        {
+            var uid = Guid.NewGuid();
+            var menu = new MenuEntity
+            {
+                Id = uid,
+                Title = request.Title.Trim(),
+                DisplayOrder = request.DisplayOrder,
+                Icon = request.Icon,
+                Url = request.Url,
+                IsOpenInNewTab = request.IsOpenInNewTab
+            };
+
+            await _context.Menu.AddAsync(menu, cancellationToken);
+            await _context.SaveChangesAsync(cancellationToken);
+
+            return new MenuDto()
+            {
+                Id = menu.Id,
+                Title = menu.Title,
+                DisplayOrder = menu.DisplayOrder,
+                Icon = menu.Icon,
+                Url = menu.Url,
+                IsOpenInNewTab = menu.IsOpenInNewTab
+            };
+        }
+    }
+}
---
+++ BioWorld.BackEnd/Controllers/MenuController.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using BioWorld.Application.Menu;
+using BioWorld.Application.Menu.Commands.CreateMenu;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace BioWorld.BackEnd.Controllers
+{
+    public class MenuController: ApiController
+    {
+        public MenuController(ILogger<ControllerBase> logger) : base(logger)
+        {
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<MenuDto>> Create(CreateMenuCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+    }
+}
